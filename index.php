bfd97843c4b437a27778102f843381e71f6576a3
= <=>
2014-12-25 17:39:42 +0700

    routing

--- index.php
+++ index.php
@@ -1,15 +1,16 @@
 <?
 require 'sistem/class_lib.php';
-$page='login';
-if(isset($_GET['page'])){
-	$page = $_GET['page'];
-}
-$sistemUtama = new Sistem();
-$src = 'tampilan/page/'.strtolower($page).".php";
-if (file_exists($src)) {
-	require $src;
-}
-else{
-	echo "We're very sorry. The page you requested doesn't exist! Back to <a href='?page=home'>home</a>.";
-}
+// $page='login';
+// if(isset($_GET['page'])){
+// 	$page = $_GET['page'];
+// }
+// $sistemUtama = new Sistem();
+// $src = 'tampilan/page/'.strtolower($page).".php";
+// if (file_exists($src)) {
+// 	require $src;
+// }
+// else{
+// 	echo "We're very sorry. The page you requested doesn't exist! Back to <a href='?page=home'>home</a>.";
+// }
+echo $_GET['url'];
 ?>

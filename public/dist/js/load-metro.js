ccad91222de2c8541e82a2c980dbb0f6c14ccdb6
= <=>
2015-01-09 22:17:13 +0700

    fixing js broken link

--- public/dist/js/load-metro.js
+++ public/dist/js/load-metro.js
@@ -1,6 +1,6 @@
 $(function(){
     if ((document.location.host.indexOf('.dev') > -1) || (document.location.host.indexOf('modernui') > -1) ) {
-        $("<script/>").attr('src', 'js/metro/metro-loader.js').appendTo($('head'));
+        $("<script/>").attr('src', 'dist/js/metro/metro-loader.js').appendTo($('head'));
     } else {
         $("<script/>").attr('src', 'dist/js/metro.min.js').appendTo($('head'));
     }

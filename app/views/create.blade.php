4211303c2b72db3eb05d8cf1e0ce2c1b28e1da6a
wiratmoko11 <wiratmoko11@gmail.com>
2015-01-12 19:06:03 -0800

    create.blade.php edited

--- app/views/create.blade.php
+++ app/views/create.blade.php
@@ -4,15 +4,37 @@
 @stop
 {{HTML::script('dist/js/ckeditor/ckeditor.js')}}
 @section('content-inner')
-	{{Form::open()}}
-		{{Form::text('title','',array('placeholder'=>'Title','class'=>'form-control','style'=>'width:200px;display:inline-block;margin-bottom:20px','required'))}}
-		{{Form::submit('submit', array('class'=>'btn btn-primary'))}}
-		{{Form::textarea('thread','')}}
-		<script>
-			CKEDITOR.replace( 'thread', {
-			    language: 'id',
-			    uiColor: 'whitesmoke'
-			});
-		</script>
-	{{Form::close()}}
+
+<div class="container">
+	<div class="grid">
+		<div class="row" style="background-color: rgba(193, 189, 194, 0.28); border: 1px solid">
+			<p style="background-color: rgba(179, 179, 185, 0.53); height:30px; margin-bottom:50px"><b style="margin-left:30px">Post New Thread</b>
+			<div class="span2" style="text-align: right">
+				<strong>Judul :</strong>
+				<br>
+				<br>
+				<p><strong>Body : </strong>
+			</div>
+			<div class="span8">
+
+				{{Form::open()}}
+				{{Form::text('title','',array('placeholder'=>'Title','class'=>'form-control','style'=>'width:200px;display:inline-block;margin-bottom:20px','required'))}}
+				
+				{{Form::textarea('thread','')}}
+				<script>
+					CKEDITOR.replace( 'thread', {
+					    language: 'id',
+					    uiColor: 'whitesmoke'
+					});
+				</script>
+				<br>
+				<br>
+				{{Form::submit('submit', array('class'=>'btn btn-primary'))}}
+				{{Form::close()}}
+			</div>
+		</div>
+	</div>
+</div>
+
+	
 @stop

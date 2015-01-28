77c7fbb5694214126d52e1c1b49c0c493de1134b
wiratmoko11 <wiratmoko11@gmail.com>
2015-01-28 11:37:54 -0800

    memisahkan poll dan categories

--- app/views/discover.blade.php
+++ app/views/discover.blade.php
@@ -8,23 +8,10 @@
 	<div class="container">
 		<div class="grid">
 			<div class="row">
-
 				<div class="col-lg-4 col-md-4">
-					<div class="sidebarblock">
-                                <h3>Categories</h3>
-                                <div class="divline"></div>
-                                <div class="blocktxt">
-                                    <ul class="cats">
-                                        <li><a href="#">Java <span class="badge pull-right">20</span></a></li>
-                                        <li><a href="#">PHP <span class="badge pull-right">10</span></a></li>
-                                        <li><a href="#">HTML <span class="badge pull-right">50</span></a></li>
-                                        <li><a href="#">Looking for Players <span class="badge pull-right">36</span></a></li>
-                                        <li><a href="#">Stupid Bugs &amp; Solves <span class="badge pull-right">41</span></a></li>
-                                        <li><a href="#">Video &amp; Audio Drivers <span class="badge pull-right">11</span></a></li>
-                                        <li><a href="#">2K Official Forums <span class="badge pull-right">5</span></a></li>
-                                    </ul>
-                                </div>
-                            </div>
+					@include('categories')
+
+					@include('poll')
 					</div>
 				<div class="col-lg-8 col-md-8">
 					@foreach($thread as $key)
--- app/views/signup.blade.php
+++ app/views/signup.blade.php
@@ -317,74 +317,10 @@
                         <div class="col-lg-4 col-md-4">
 
                             <!-- -->
-                            <div class="sidebarblock">
-                                <h3>Categories</h3>
-                                <div class="divline"></div>
-                                <div class="blocktxt">
-                                    <ul class="cats">
-                                        <li><a href="#">Trading for Money <span class="badge pull-right">20</span></a></li>
-                                        <li><a href="#">Vault Keys Giveway <span class="badge pull-right">10</span></a></li>
-                                        <li><a href="#">Misc Guns Locations <span class="badge pull-right">50</span></a></li>
-                                        <li><a href="#">Looking for Players <span class="badge pull-right">36</span></a></li>
-                                        <li><a href="#">Stupid Bugs &amp; Solves <span class="badge pull-right">41</span></a></li>
-                                        <li><a href="#">Video &amp; Audio Drivers <span class="badge pull-right">11</span></a></li>
-                                        <li><a href="#">2K Official Forums <span class="badge pull-right">5</span></a></li>
-                                    </ul>
-                                </div>
-                            </div>
+                            @include('categories')
 
                             <!-- -->
-                            <div class="sidebarblock">
-                                <h3>Poll of the Week</h3>
-                                <div class="divline"></div>
-                                <div class="blocktxt">
-                                    <p>Which game you are playing this week?</p>
-                                    <form action="#" method="post" class="form">
-                                        <table class="poll">
-                                            <tbody><tr>
-                                                <td>
-                                                    <div class="progress">
-                                                        <div class="progress-bar color1" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 90%">
-                                                            Call of Duty Ghosts
-                                                        </div>
-                                                    </div>
-                                                </td>
-                                                <td class="chbox">
-                                                    <input id="opt1" name="opt" value="1" type="radio">  
-                                                    <label for="opt1"></label>  
-                                                </td>
-                                            </tr>
-                                            <tr>
-                                                <td>
-                                                    <div class="progress">
-                                                        <div class="progress-bar color2" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 63%">
-                                                            Titanfall
-                                                        </div>
-                                                    </div>
-                                                </td>
-                                                <td class="chbox">
-                                                    <input id="opt2" name="opt" value="2" checked="checked" type="radio">  
-                                                    <label for="opt2"></label>  
-                                                </td>
-                                            </tr>
-                                            <tr>
-                                                <td>
-                                                    <div class="progress">
-                                                        <div class="progress-bar color3" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 75%">
-                                                            Battlefield 4
-                                                        </div>
-                                                    </div>
-                                                </td>
-                                                <td class="chbox">
-                                                    <input id="opt3" name="opt" value="3" type="radio">  
-                                                    <label for="opt3"></label>  
-                                                </td>
-                                            </tr>
-                                        </tbody></table>
-                                    </form>
-                                    <p class="smal">Voting ends on 19th of October</p>
-                                </div>
-                            </div>
+                            @include('poll')
 
                             <!-- -->
                             <div class="sidebarblock">
